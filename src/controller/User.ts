9f3d64655d655625b7c68716f8c4e5867d468c91
Trungqph09417 <bqtrung10a10kx1@gmail.com>
2025-06-12 22:16:38 +0700

    loginwithgoogle

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import { getAccesToken } from "../utils/getAccesToken";
 import { log } from "node:console";
+import { ganeratorRandomText } from "../utils/ganeratorRandomText";
 dotenv.config();
 const register = async (req: any, res: any) => {
   const body = req.body;
@@ -43,7 +44,60 @@ const register = async (req: any, res: any) => {
     });
   }
 };
+const SignWithGoogle = async (req: any, res: any) => {
+  const body = req.body;
+  const { email, name } = body;
+  try {
+    const user: any = await UserModel.findOne({ email });
+    // console.log("keo qua tim kiem user", user);
 
+    if (user) {
+      const userObj = user.toObject ? user.toObject() : { ...user };
+      delete userObj.password;
+
+      console.log("thong tin user SignWithGoogle ", user);
+
+      res.status(200).json({
+        message: "Login successfyly!",
+        data: {
+          ...userObj,
+          token: await getAccesToken({
+            _id: user._id,
+            email: user.email,
+            rules: user.rule ?? 1,
+          }),
+        },
+      });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashpassword = await bcrypt.hash(ganeratorRandomText(6), salt);
+
+      body.password = hashpassword;
+
+      const newUser = new UserModel(body);
+      await newUser.save();
+
+      const userResponse = newUser.toJSON() as { [key: string]: any };
+      delete userResponse.password;
+
+      userResponse.token = await getAccesToken({
+        _id: userResponse._id,
+        email: userResponse.email,
+        rules: 1,
+      });
+      console.log("ham user ", userResponse);
+
+      res.status(200).json({
+        message: "Register",
+        data: userResponse,
+      });
+    }
+  } catch (error: any) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
 const Login = async (req: any, res: any) => {
   const body = req.body;
   const { email, password } = body;
@@ -56,7 +110,9 @@ const Login = async (req: any, res: any) => {
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Mật khẩu của bạn không đúng!" });
+      return res.status(400).json({
+        message: "Dăng nhập thất bại! Vui lòng kiểm tra lại Email/Password",
+      });
     }
     const userResponse = user.toObject() as {
       [key: string]: any;
@@ -82,4 +138,4 @@ const Login = async (req: any, res: any) => {
   }
 };
 
-export { register, Login };
+export { register, Login, SignWithGoogle };
--- src/routers/User.ts
+++ src/routers/User.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { Login, register } from "../controller/User";
+import { Login, register, SignWithGoogle } from "../controller/User";
 
 const router = Router();
 router.post("/register", register);
 router.post("/login", Login);
+router.post("/google-login", SignWithGoogle);
 export default router;
